e3fcf116e4b8dffbb3a5c5b1a6c230e119de0f16
marioOrtegaGarcia <mario.j.ortega.garcia@gmail.com>
2019-03-02 15:18:31 -0800

    Messing with Image Handle Class

--- projectzero MessagesExtension/hangleImage.swift
+++ projectzero MessagesExtension/hangleImage.swift
@@ -15,4 +15,37 @@ class handleImage: NSObject {
     fileprivate var currentVC: UIViewController!
     
     // MARK:
+    var imageBlock: ((UIImage) -> Void)?
+    
+    // Load Camera to take Picture
+    func loadCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraController = UIImagePickerController()
+            cameraController.delegate = (self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate);
+            cameraController.sourceType = .camera
+            currentVC.present(cameraController, animated: true, completion: nil)
+        }
+    }
+    
+    // Load Image from the library
+    func loadFromLibrary() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let libraryController = UIImagePickerController()
+            libraryController.delegate = (self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate);
+            libraryController.sourceType = .photoLibrary
+            currentVC.present(libraryController, animated: true, completion: nil)
+        }
+    }
+    
+    func showActionSheet(vc: UIViewController) {
+        currentVC = vc
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        actionSheet.addAction(
+            UIAlertAction(title: "Camera", style: .default, handler: {
+                (alert:UIAlertAction!) -> Void in self.loadCamera()
+            })
+        )
+    }
+    
+    
 }

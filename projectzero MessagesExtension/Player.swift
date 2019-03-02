571463ac999ae8a9841b1294ee2b537c919b223e
alsendor <al_nucum@yahoo.com>
2019-03-01 22:42:14 -0800

    Added player class

---
+++ projectzero MessagesExtension/Player.swift
@@ -0,0 +1,25 @@
+//
+//  Player.swift
+//  projectzero MessagesExtension
+//
+//  Created by Alsendor Nucum on 3/1/19.
+//  Copyright © 2019 Alsendor Nucum. All rights reserved.
+//
+
+import Foundation
+
+class Player {
+    
+    var name: String
+    var id: Int
+    var score: Int
+    var king: Bool
+    
+    init(playerName: String) {
+        self.name = playerName
+        self.score = 0
+        self.king = false //will start random king later
+        self.id = 1 //will assign player ids later on
+    }
+    
+}
